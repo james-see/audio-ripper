35189bfcddf2794a9c5df4c9e55db807e68496cf
JC <james@jamescampbell.us>
2022-02-01 14:58:01 -0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-"""Parameters to set that should be copied to config.py which is gitignored"""
-# example_config.py
-bearertoken = "BQCQhECZVkiGnbQulMW7vhefQ1uVhSfJdxxSQzdRU3xUSSTUal8topdDsOQWW5RUPIi1JZKgPmYRGh-Wwm7669GEGqFKm1k5RdsyZ6gQSAa0XEN-J_kOA874JLDxWDUuHDzZBdhxWv0dOwMd0IZpaN5LW0EM"
-playlistid = "4bAnOAgblJnWhMYSQWbZHL"
